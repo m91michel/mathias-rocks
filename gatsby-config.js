72254bd5c8d7a51ee8af1d6310e93ef7fd78e53d
Mathias Michel <m.michel@klarx.com>
2021-02-07 19:49:41 +0100

    add tailwind css

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,7 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-typescript`,
+    `gatsby-plugin-postcss`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
